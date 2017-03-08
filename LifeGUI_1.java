1348c9726c86303157fc978581f6f977ccd18fe2
Danny Yi <dannyyipatroller@gmail.com>
2017-03-08 08:25:48 -0800

    Add files via upload

---
+++ LifeGUI_1.java
@@ -0,0 +1,153 @@
+/*
+ * Danny Yi March 5th 2017 Period 4
+ * This lab took me approximately 60 minutes.
+ * After I completed this lab, I felt really accomplished and felt like I could 
+ * potentially do a lot more. It is definitely getting a lot interesting once I
+ * know what I'm doing. I learned a solution to how to fix the problem of HW#3
+ * GUI of fixing the complete checkerboard. I should've realized that using 2D
+ * arrays would be good for making the board and easy to adjust the slide with
+ * a slider. In this lab that's how I did it, by adjusting each panel size.
+ */
+
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class LifeGUI_1 extends Application{
+
+	private LifeModel model;
+	private BooleanGridPane gridPane;
+	private Button clear;
+	private Button load;
+	private Button nextGen;
+	Scanner in;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		stage.setTitle("Grid");
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 500, 500);
+		gridPane = new BooleanGridPane();
+		
+		Boolean[][] data = {{false, false, false, false},
+							{false, false, false, false},
+							{false, false, false, false},
+							{false, false, false, false},
+							{false, false, false, false},
+							{false, false, false, false}}; 
+		
+		model = new LifeModel(data);
+		gridPane.setModel(model);
+		gridPane.setOnMouseClicked(new GridMouseHandler());
+		
+		Slider slide = new Slider(0, 50, 10);
+		slide.setShowTickMarks(true);
+		slide.setShowTickLabels(true);
+		slide.setMajorTickUnit(5);
+		slide.valueProperty().addListener(new SlideHandler());
+		
+		HBox hbox = new HBox();
+		clear = new Button("Clear");
+		clear.setOnMouseClicked(new ButtonHandler());
+		load = new Button("Load");
+		load.setOnMouseClicked(new ButtonHandler());
+		nextGen = new Button ("Next Generation");
+		nextGen.setOnMouseClicked(new ButtonHandler());
+		
+		Label gen = new Label("Generation " + gridPane.getModel().getGeneration());
+		
+		hbox.getChildren().addAll(clear, load, nextGen, gen);
+		
+		root.setTop(hbox);
+		root.setCenter(gridPane);
+		root.setBottom(slide);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	private class GridMouseHandler implements EventHandler<MouseEvent>{
+		@Override
+		public void handle(MouseEvent e) {
+			int row = gridPane.rowForYPos(e.getY());
+			int col = gridPane.colForXPos(e.getX());
+			gridPane.getModel().setValueAt(row, col, !gridPane.getModel().getValueAt(row, col));
+		}
+	}
+	
+	private class SlideHandler implements ChangeListener<Number>{
+
+		@Override
+		public void changed(ObservableValue<? extends Number> observable,
+				Number oldValue, Number newValue) {
+			gridPane.setTileSize((Double)newValue);
+		}
+		
+	}
+	
+	private class ButtonHandler implements EventHandler<MouseEvent> {
+
+		@Override
+		public void handle(MouseEvent e) {
+			if(e.getSource() == clear){
+				for(int i = 0; i < gridPane.getModel().getNumRows(); i++){
+					for(int j = 0; j < gridPane.getModel().getNumCols(); j++){
+						if(gridPane.getModel().getValueAt(i, j)){
+							gridPane.getModel().setValueAt(i, j, false);
+						}
+					}
+				}
+			}else if(e.getSource() == load){
+				FileDialog fd = new FileDialog(new Frame(), "Select a Color database", FileDialog.LOAD);
+				fd.setVisible(true);
+				
+				try {
+					ArrayList<String> list = new ArrayList<String>();
+					in = new Scanner(new File(fd.getDirectory() + fd.getFile()));
+					while(in.hasNext()){
+						list.add(in.nextLine());
+					}
+					Boolean[][] newData = new Boolean[list.size()][list.get(0).length() / 2 + 1];
+					for(int row = 0; row < newData.length; row++){
+						for(int col = 0; col < list.get(row).length(); col++){
+							if(list.get(row).charAt(col) == 'x'){
+								newData[row][col / 2] = false;
+							}else if(list.get(row).charAt(col) == 'o'){
+								newData[row][col / 2] = true;
+							}
+						}
+					}
+					gridPane.setModel(new LifeModel(newData));
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+			}else if(e.getSource() == nextGen){
+				gridPane.getModel().nextGeneration();
+			}
+		}
+		
+	}
+	
+}
